bcbb2d6722098ce6a89c282d1b74096b899dde46
Quentin Gautreau <qgautreau@gmail.com>
2017-02-10 11:10:10 +0100

    grille ok, boutons à placer correctement

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 var rowsNumber = 6;
 var columnsNumber = 7;
 var boardElement = document.getElementById('board');
-for (var i =0; i < rowsNumber; i++) {
+
+for (var i = 0; i < rowsNumber; i++) {
     for (var j = 0; j < columnsNumber; j++) {
-    var gridElement = document.createElement("table");
-    gridElement.className = "grid";
-    boardElement.appendChild(gridElement);
-    gridElement.addEventListener("click", function (){
-        this.style.backgroundColor = "red";
-    });
+    var caseElement = document.createElement("div");
+    caseElement.className = "case";
+    caseElement.id = "case"+i+"-"+j;
+    boardElement.appendChild(caseElement);
     }
 }
+
+for (var i = 0; i < columnsNumber; i++){
+var dropButtonElement = document.createElement("button");
+dropButtonElement.id = "dropbutton"+i+"-"+j;
+boardElement.appendChild(dropButtonElement);
+}
+
+var grid = [["", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", ""]];
